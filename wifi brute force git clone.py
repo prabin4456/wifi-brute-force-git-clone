9a1fd3260c620d5ea5ef9b7df57ce720c6c7178c
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-28 09:24:13 +0545

    Update wifi brute force git clone.py

--- wifi brute force git clone.py
+++ wifi brute force git clone.py
@@ -2,22 +2,16 @@ git clone https://github.com/n00py/WPForce
 git clone https://github.com/cinquemb/WifiBruteCrack.git
 git clone https://github.com/P0cL4bs/WiFi-Pumpkin.git
 git clone https://github.com/RecepGunes01/WordPress-Brute-Force
-https://github.com/1N3/BruteX
-https://github.com/FluxionNetwork/fluxion
-https://github.com/FluxionNetwork/fluxion
-
-https://github.com/derv82/wifite2
-
-https://github.com/aircrack-ng/aircrack-ng
-
-https://github.com/threat9/routersploit
-
-https://github.com/wifiphisher/wifiphisher
-
-https://github.com/WiPi-Hunter/PiDense
-
-https://github.com/aress31/wirespy
-
-https://github.com/3xploitGuy/lazyaircrack
-
-https://github.com/MS-WEB-BN/h4rpy
+git clone https://github.com/1N3/BruteX
+git clone https://github.com/FluxionNetwork/fluxion
+git clone https://github.com/awkman/pywifi.git
+git clone https://github.com/53k41iga/wpa2-psk-brute-force.git
+git clone https://github.com/madeindjs/Wifi_BruteForce.git
+git clone https://github.com/derv82/wifite2
+git clone https://github.com/aircrack-ng/aircrack-ng
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/wifiphisher/wifiphisher
+git clone https://github.com/WiPi-Hunter/PiDense
+git clone https://github.com/aress31/wirespy
+git clone https://github.com/3xploitGuy/lazyaircrack
+git clone https://github.com/MS-WEB-BN/h4rpy
