b97e5c69053fe1f0cdfc002791b1146a20a752a9
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-28 09:07:48 +0545

    Update wifi brute force git clone.py

--- wifi brute force git clone.py
+++ wifi brute force git clone.py
@@ -4,3 +4,20 @@ git clone https://github.com/P0cL4bs/WiFi-Pumpkin.git
 git clone https://github.com/RecepGunes01/WordPress-Brute-Force
 https://github.com/1N3/BruteX
 https://github.com/FluxionNetwork/fluxion
+https://github.com/FluxionNetwork/fluxion
+
+https://github.com/derv82/wifite2
+
+https://github.com/aircrack-ng/aircrack-ng
+
+https://github.com/threat9/routersploit
+
+https://github.com/wifiphisher/wifiphisher
+
+https://github.com/WiPi-Hunter/PiDense
+
+https://github.com/aress31/wirespy
+
+https://github.com/3xploitGuy/lazyaircrack
+
+https://github.com/MS-WEB-BN/h4rpy
